89739226306df170ad28cc0f866febc49d9c55f4
TIRED FOX <80540019+DeFFeCT139@users.noreply.github.com>
2025-05-24 16:24:12 +0300

    add new rest api from api

--- api/next.config.mjs
+++ api/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    experimental: {
+        serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+    },
+}
+
 export default nextConfig;
